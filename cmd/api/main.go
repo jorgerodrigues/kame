1452effa319f615b94c57fad5b2ab9110b319125
Jorge Lopes Rodrigues <jorgelopes@hey.com>
2023-12-30 11:33:03 +0000

    Server running

---
+++ cmd/api/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/jorgerodrigues/upkame/internal/server"
+)
+
+func main() {
+
+	server := server.NewServer()
+
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(fmt.Sprintf("cannot start server: %s", err))
+	}
+}
