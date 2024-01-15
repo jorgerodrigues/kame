3f3e6018196ecb8efb2ac976d77992825d1bf02a
Jorge Lopes Rodrigues <jorgelopes@hey.com>
2024-01-15 06:19:46 +0100

    Graceful shutdown

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,18 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"os"
-
 	"github.com/jorgerodrigues/upkame/internal/server"
 )
 
 func main() {
 
-	server := server.NewServer()
-	fmt.Printf("Server running on port %v 🚀 \n", os.Getenv("PORT"))
-	err := server.ListenAndServe()
-	if err != nil {
-		panic(fmt.Sprintf("cannot start server: %s", err))
-	}
+  server.StartServer()
 }
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,10 +1,14 @@
 package server
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
+	"os/signal"
 	"strconv"
+	"syscall"
 	"time"
 
 	_ "github.com/joho/godotenv/autoload"
@@ -39,4 +43,41 @@ func NewServer() *http.Server {
 	}
 
 	return server
+}
+
+func StartServer() {
+	logger := logger.NewLogger()
+	// Create a new server
+	server := NewServer()
+
+	// Use a WaitGroup to wait for the server to start
+
+	// Start the server
+	go func() {
+		logger.Info("🔥Server is starting on port :", server.Addr, "")
+		err := server.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
+			log.Fatalf("ListenAndServe(): %s", err)
+		}
+	}()
+
+	// Create a channel to listen for interrupt signals
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt, syscall.SIGTERM, syscall.SIGABRT)
+
+	// Block until a signal is received
+	sig := <-quit
+	logger.Warn("Shutting down server... Reason: %v", sig)
+
+	// Create a deadline for the shutdown
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// Attempt to gracefully shut down the server
+	err := server.Shutdown(ctx)
+	if err != nil {
+		logger.Error("Server forced to shutdown: %v", err)
+	}
+
+	logger.Info("Server exiting")
 }
